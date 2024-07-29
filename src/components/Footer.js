ca71462f698f2325d9dc178c15a7625a83bc53ee
Deadstar III <deadstar@Deadstars-Mac-mini.local>
2024-07-29 09:03:10 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         className="w-full border-b-2 border-gray-800"
         style={{ maxHeight: "1vh" }}></div>
       <footer className="bg-gray-900 text-white text-center py-6">
-        <p>&copy; {new Date().getFullYear()} WHSF. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} WHSF</p>
       </footer>
     </div>
   );
